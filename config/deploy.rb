ccd2e3d7d952488a0bed40ee00b41a93290e59d6
arvind <apjanvekar@gmail.com>
2018-04-02 17:53:37 +0530

    server ip changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
 # config valid only for current version of Capistrano
 lock '3.10.1'
 
-set :application, 'deploy_app'
+set :application, 'test_user'
 set :repo_url, 'git@github.com:apjanvekar/deploy_app_test.git' # Edit this to match your repository
 set :branch, :master
-set :deploy_to, '/home/deploy/urlshortner'
+set :deploy_to, '/home/deploy/deploy_app'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
