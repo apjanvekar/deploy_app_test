62fd5cc5e8530fb5da656b619d77f38d22b3684c
arvind <apjanvekar@gmail.com>
2018-04-04 15:16:47 +0530

    added new configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,8 @@
-# config valid only for current version of Capistrano
-lock '3.10.1'
-
-# set :application, 'deploy_app'
-# set :repo_url, 'git@github.com:apjanvekar/deploy_app_test.git' # Edit this to match your repository
-# set :branch, :master
-# set :deploy_to, '/home/appuser/deploy_app'
-##########################################################################
-
 # Change these
 server '104.239.168.252', port: 9292, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:apjanvekar/deploy_app_test.git'
-set :application,     'test_app'
+set :application,     'deploy_test'
 set :user,            'appuser'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
@@ -33,8 +24,8 @@ set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 ## Defaults:
-set :scm,           :git
-set :branch,        :master
+# set :scm,           :git
+# set :branch,        :master
 # set :format,        :pretty
 # set :log_level,     :debug
 # set :keep_releases, 5
@@ -91,71 +82,3 @@ end
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
 # kill -s SIGTERM pid   # Stop puma
-
-#########################################################################33
-# set :pty, true
-# set :linked_files, %w{config/database.yml config/application.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
-# set :keep_releases, 5
-# set :rvm_type, :user
-# set :rvm_ruby_version, 'ruby-2.2.2' # Edit this if you are using MRI Ruby
-
-# set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-# set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-# set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-# set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
-# set :puma_conf, "#{shared_path}/puma.rb"
-# set :puma_access_log, "#{shared_path}/log/puma_error.log"
-# set :puma_error_log, "#{shared_path}/log/puma_access.log"
-# set :puma_role, :app
-# set :puma_env, fetch(:rack_env, fetch(:rails_env, 'development'))
-# set :puma_threads, [0, 8]
-# set :puma_workers, 0
-# set :puma_worker_timeout, nil
-# set :puma_init_active_record, true
-# set :puma_preload_app, false
-
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# namespace :deploy do
-
-#   after :restart, :clear_cache do
-#     on roles(:web), in: :groups, limit: 3, wait: 10 do
-#       # Here we can do anything such as:
-#       # within release_path do
-#       #   execute :rake, 'cache:clear'
-#       # end
-#     end
-#   end
-
-#end
